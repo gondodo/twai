329192c615b4b9ff659ff11074b73c4239dadd3f
whygondo <snowsmile.1217@gmail.com>
2016-12-02 23:40:31 +0900

    add account information and create db

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -4,32 +4,53 @@ require 'mechanize'
 
 class Bird < ActiveRecord::Base
   def self.search(post)
-    url = URI.parse("https://twitter.com/search?")
-    charset = nil
-    url.query = {
-      vertical: "default",
-      q: post,
-      src:"typd",
-      lang: "ja"
-    }.to_param
-
-    charset = nil
-    html = open(url) do |f|
-      charset = f.charset # 文字種別を取得
-      f.read # htmlを読み込んで変数htmlに渡す
-    end
-
+    # クエリするURLを生成
+    url = Bird.create_url(post)
     # htmlをパース(解析)してオブジェクトを生成
-    doc = Nokogiri::HTML.parse(html, nil, charset)
+    doc = Bird.open_url(url)
 
     tweets_list = []
-    doc.xpath("//ol[@id='stream-items-id']/li/div/div[2]/div/p").each do |tweet|
-      tweets_list << tweet.inner_text
+    logger.info("取得したツイート数": doc.xpath("//ol[@id='stream-items-id']/li/div/div[2]").size)
+    doc.xpath("//ol[@id='stream-items-id']/li/div/div[2]").each do |tweet|
+      tweet_hash = {
+        account: tweet.xpath("div/a/span[2]").inner_text,
+        tweet: tweet.xpath("div/p").inner_text
+      }
+      tweets_list << tweet_hash
     end
+    logger.info(tweets_list)
+
     if tweets_list.present?
-      tweets_list[rand(tweets_list.size)]
+      reply = tweets_list[rand(tweets_list.size)]
     else
-      "せやなー、知らんけどｗ"
+      reply = {
+        account: "なし",
+        tweet: "せやなー、知らんけどｗ"
+      }
     end
+    bird = Bird.create(account: reply[:account], tweet:reply[:tweet] , post: post)
+    bird.tweet
   end
+
+  private
+    def self.create_url(post)
+      url = URI.parse("https://twitter.com/search?")
+      charset = nil
+      url.query = {
+        vertical: "default",
+        q: post,
+        src:"typd",
+        lang: "ja"
+      }.to_param
+      url
+    end
+
+    def self.open_url(url)
+      charset = nil
+      html = open(url) do |f|
+        charset = f.charset # 文字種別を取得
+        f.read # htmlを読み込んで変数htmlに渡す
+      end
+      doc = Nokogiri::HTML.parse(html, nil, charset)
+    end
 end
