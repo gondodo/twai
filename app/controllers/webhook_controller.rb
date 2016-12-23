0db2c938edbb2ea7fe43f3c8010918e37c9af21b
gondodo <snowsmile.1217@gmail.com>
2016-12-23 20:52:11 +0900

    kansai ver

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -14,95 +14,45 @@ class WebhookController < ApplicationController
 
     logger.info(params)
     event = params["events"][0]
-    logger.info({from_line: event})
+    # 取得したテキスト
+    text_message = event["message"]["text"]
+    # 送ってきたユーザID
+    mid = event['source']['userId']
 
-    # if result['content']['opType'].present?
-    #
-    #   mid = result['content']['params'][0]
-    #
-    #   client = LineClient.new(CHANNEL_ID, CHANNEL_SECRET, CHANNEL_MID, OUTBOUND_PROXY)
-    #   res = client.profile(mid)
-    #
-    #   if res.status == 200
-    #     logger.info({success: res})
-    #
-    #     display_name = res.body['contacts'][0]['displayName']
-    #
-    #     opType = result['content']['opType']
-    #     case opType
-    #     when 4 then
-    #       user = User.create_with(display_name: display_name, status: 4).find_or_create_by(mid: mid)
-    #       user.display_name = display_name
-    #       user.status = 4
-    #       user.save!
-    #     when 8 then
-    #       user = User.create_with(display_name: display_name, status: 8).find_or_create_by(mid: mid)
-    #       user.display_name = display_name
-    #       user.status = 8
-    #       user.save!
-    #     else
-    #     end
-    #   else
-    #     logger.info({fail: res})
-    #   end
-    # else
+    replyToken = event['replyToken']
 
-    # event = params["events"][0]
-    # event_type = event["type"]
-    # replyToken = event["replyToken"]
-    #
-    # case event_type
-    # when "message"
-    #   input_text = event["message"]["text"]
-    #   tweet = Bird.search(input_text)
-    #   output_text = tweet
-    # end
-    #
-      # 取得したテキスト
-      text_message = event["message"]["text"]
-      # 送ってきたユーザID
-      mid = event['source']['userId']
-      # replyToken
-      replyToken = event['replyToken']
-      
-      logger.info(mid)
+    if User.find_by(mid: mid) == nil
+      user = User.create(mid: mid)
+    else
+      user = User.find_by(mid: mid)
+    end
+    Message.create(user_id: user.id, text_message: text_message)
 
-      if User.find_by(mid: mid) == nil
-        user = User.create(mid: mid)
-      else
-        user = User.find_by(mid: mid)
-      end
-      Message.create(user_id: user.id, text_message: text_message)
+    ### ここから修正 ###
+    docomo_client = DocomoClient.new(DOCOMO_API_KEY)
+    response = nil
+    last_dialogue_info = LastDialogueInfo.find_by(mid: mid)
+    if last_dialogue_info.nil?
+      response =  docomo_client.dialogue(text_message)
+      last_dialogue_info = LastDialogueInfo.new(mid: mid, mode: response.body['mode'], da: response.body['da'], context: response.body['context'])
+    else
+      response =  docomo_client.dialogue(text_message, last_dialogue_info.mode, last_dialogue_info.context, t: 20)
+      last_dialogue_info.mode = response.body['mode']
+      last_dialogue_info.da = response.body['da']
+      last_dialogue_info.context = response.body['context']
+    end
+    last_dialogue_info.save!
+    message = response.body['utt']
+    ### ここまで修正 ###
 
-      ### ここから修正 ###
-      docomo_client = DocomoClient.new(DOCOMO_API_KEY)
-      response = nil
-      last_dialogue_info = LastDialogueInfo.find_by(mid: mid)
-      if last_dialogue_info.nil?
-        response =  docomo_client.dialogue(text_message)
-        last_dialogue_info = LastDialogueInfo.new(mid: mid, mode: response.body['mode'], da: response.body['da'], context: response.body['context'])
-      else
-        response =  docomo_client.dialogue(text_message, last_dialogue_info.mode, last_dialogue_info.context)
-        last_dialogue_info.mode = response.body['mode']
-        last_dialogue_info.da = response.body['da']
-        last_dialogue_info.context = response.body['context']
-      end
-      last_dialogue_info.save!
-      logger.info("------------------------------")
-      logger.info(response.body)
-      message = response.body['utt']
-      ### ここまで修正 ###
+    client = LineClient.new(CHANNEL_ACCESS_TOKEN, OUTBOUND_PROXY)
+    res = client.reply(replyToken, message)
 
-      client = LineClient.new(CHANNEL_ACCESS_TOKEN, OUTBOUND_PROXY)
-      # res = client.send(mid, message)
-      res = client.reply(replyToken, message)
-
-      if res.status == 200
-        logger.info({success: res})
-      else
-        logger.info({fail: res})
-      end
-    # end
+    if res.status == 200
+      logger.info({success: res})
+    else
+      logger.info({fail: res})
+    end
 
     render :nothing => true, status: :ok
   end
